c25ae43baa2fc1b4a586ba7f052175560e21a067
Carlos Peralta <carlos9917@gmail.com>
2025-10-09 12:53:30 +0000

    added tests and list of tif files

--- scripts/test_long.sh
+++ scripts/test_long.sh
@@ -2,20 +2,18 @@
 # Quick test script with just 5 stations
 set -e
 
-echo "Creating test CSV with 5 stations..."
-#head -5 /mnt/drive/hirtst/DSM_DK/station_data_20250824_utm.csv > /tmp/test_5_stations.csv
-head -100 tests/station_data_20250824_utm.csv > /tmp/test_5_stations.csv
-
-echo "Running parallel processing with 2 workers..."
+CSV=/mnt/drive/hirtst/terrain-shadow-processor/tests/still_missing_20230124_utm.csv
+NP=10
+echo "Running parallel processing with $NP workers..."
 source /mnt/drive/hirtst/python-shadows/bin/activate
 source /mnt/drive/hirtst/terrain-shadow-processor/env.sh
 
 $PYBIN /mnt/drive/hirtst/terrain-shadow-processor/src/run_parallel_processing.py \
-    --csv /tmp/test_5_stations.csv \
+    --csv $CSV \
     --config /mnt/drive/hirtst/terrain-shadow-processor/config/shadows_conf.ini \
-    --workers 2 \
-    --output-dir /tmp/test_output \
-    --log-dir /tmp/test_logs \
+    --workers $NP \
+    --output-dir /mnt/drive/hirtst/terrain-shadow-processor/data \
+    --log-dir /mnt/drive/hirtst/terrain-shadow-processor/logs \
     --tiles-dir ${DSMPATH} \
     --work-dir /tmp/test_work \
     --type road
